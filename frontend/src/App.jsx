1e2db5edd225e646febe49ab68ca575a651e4770
Yash Kumar <hypnoastic@gmail.com>
2025-04-10 11:04:25 +0530

    deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import MainSection from './components/MainSection';
 import About from './components/About';
 import Footer from './components/Footer';

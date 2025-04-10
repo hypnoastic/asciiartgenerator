9223b086aa70fe630576b62199a395e2f5f3b2ec
Yash Kumar <hypnoastic@gmail.com>
2025-04-10 09:44:03 +0530

    deploy

--- backend/server.py
+++ backend/server.py
@@ -1,28 +1,23 @@
 from flask import Flask, request, send_file
-from ascii import image_to_highres_ascii_image  # Import your image processing logic
+from flask_cors import CORS
+from ascii import image_to_highres_ascii_image
 
 app = Flask(__name__)
 
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limit to 16MB
-
+# Enable CORS for all routes and origins
+CORS(app)
 
 @app.route('/upload/', methods=['POST'])
 def upload_image():
-    if 'file' not in request.files:
-        return "No file part", 400
-
     file = request.files['file']
-
-    # Check if the user has selected a file
-    if file.filename == '':
-        return "No selected file", 400
-
     image_bytes = file.read()
 
+    # Generate ASCII image in memory
     ascii_buffer = image_to_highres_ascii_image(image_bytes)
 
+    # Send the image as a response
+    # Since ascii_buffer is already a BytesIO object, we can directly use it in send_file
     return send_file(ascii_buffer, mimetype='image/png')
 
-
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
